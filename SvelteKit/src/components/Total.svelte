d887aa5bb04348737834db9dc18545212d0a58df
yoo-jimin127 <dbwlals9936@naver.com>
2023-01-31 20:49:24 +0900

    add : Total

--- SvelteKit/src/components/Total.svelte
+++ SvelteKit/src/components/Total.svelte
@@ -2,14 +2,30 @@
 	import { todoData } from "../store";
 
 
-    let completedTodo = $todoData.filter((todo) => todo.completed).length
-    let ratio = $todoData.length > 0 ? Number((completedTodo / $todoData.length).toFixed(2)) : 0
+    $: completedTodo = $todoData.filter((todo) => todo.completed).length
+    $: ratio = $todoData.length > 0 ? Number((completedTodo / $todoData.length).toFixed(2)) : 0
 </script>
 
 <div>
     Total :
     <span>
-        <b>{ratio}</b>
+        <b>{ratio * 100}</b>
         %
     </span>
 </div>
+
+<style>
+	div {
+		border-top: 2px dashed;
+		padding: 1rem 0;
+		display: flex;
+		justify-content: space-between;
+		font-size: 1.2rem;
+	}
+	span {
+		color: rgb(110, 44, 243);
+	}
+	b {
+		font-size: 1.4rem;
+	}
+</style>
