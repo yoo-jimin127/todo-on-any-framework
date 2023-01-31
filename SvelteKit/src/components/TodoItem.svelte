02c8750ece45db8fcf8ac7a2442fbd282baf468b
yoo-jimin127 <dbwlals9936@naver.com>
2023-01-31 20:39:25 +0900

    add : todoItem component

---
+++ SvelteKit/src/components/TodoItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { todoData } from "../store";
+
+    export let todo;
+    let {id, text, completed} = todo;
+
+    const handleRemoveTodo = (id: number) => {
+        $todoData = $todoData.filter((todo) => todo.id === id)
+    };
+</script>
+
+<div>
+    <label>
+        <input type="checkbox" bind:checked={completed} />
+        {text}
+    </label>
+    <button>x</button>
+</div>
