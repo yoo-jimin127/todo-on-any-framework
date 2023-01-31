b54b6d3ec976804e57d5cc911e8c43fea2047817
yoo-jimin127 <dbwlals9936@naver.com>
2023-01-31 20:41:50 +0900

    add : todoItem component - complete

--- SvelteKit/src/components/TodoItem.svelte
+++ SvelteKit/src/components/TodoItem.svelte
@@ -7,12 +7,17 @@
     const handleRemoveTodo = (id: number) => {
         $todoData = $todoData.filter((todo) => todo.id === id)
     };
+
+    const handleCompleteTodo = (id: number) => {
+        const index = $todoData.findIndex((todo) => todo.id === id)
+        $todoData[index].completed = !$todoData[index].completed;
+    };
 </script>
 
 <div>
     <label>
-        <input type="checkbox" bind:checked={completed} />
+        <input type="checkbox" bind:checked={completed} on:click={() => handleCompleteTodo(id)} />
         {text}
     </label>
-    <button>x</button>
+    <button on:click={() => handleRemoveTodo(id)}>x</button>
 </div>
