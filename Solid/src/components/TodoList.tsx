15b26bb266717d6dd525c1fd72304ea54686db0f
yoo-jimin127 <dbwlals9936@naver.com>
2023-02-14 20:34:38 +0900

    add : TodoList For each

--- Solid/src/components/TodoList.tsx
+++ Solid/src/components/TodoList.tsx
@@ -6,14 +6,30 @@ import styles from "./TodoList.module.css";
 
 interface TodoListProps {
     todos: Todo[];
+    onItemDone?: (index: number) => void;
+    onItemUpPress?: (index: number) => void;
+    onItemDownPress?: (index: number) => void;
+    onItemDelete?: (index: number) => void; 
 }
+
 const TodoList = (props: TodoListProps) => {
     return (
         <div class={styles.container}>
             <For each={props.todos}> 
                 {/* 인덱스가 바뀔 일이 있을 때 사용 */}
                 {(todo, index) => (
-                    <TodoItem {...todo} />
+                    <TodoItem {...todo} onDone={() => {
+                        props.onItemDone?.(index());
+                    }}
+                    onUpPress={() => {
+                        props.onItemUpPress?.(index());
+                    }}
+                    onDownPress={() => {
+                        props.onItemDownPress?.(index());
+                    }}
+                    onDelete={() => {
+                        props.onItemDelete?.(index());
+                    }} />
                 )}
             </For>
             {}
